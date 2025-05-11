86501fd939e79cd58ffd2a2f225400ed425b7f92
Dmitri_Sidorov <kin19861@rambler.ru>
2025-05-11 16:42:32 +0300

    add func "add_product" and getter

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Создание класса категории"""
 
@@ -7,9 +10,27 @@ class Category:
     category_count = 0  # Количество категорий
     product_count = 0  # Количество товаров
 
+
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product) :
+        """Метод добавления нового продукта"""
+        if not isinstance(product, Product):
+            raise TypeError("Можно добавлять только объекты класса Product или его наследников")
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        """Геттер с функцией вывода товаров"""
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
