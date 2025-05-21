6976ca4b34b6d31286d3cca35da695556073a630
Dmitri_Sidorov <kin19861@rambler.ru>
2025-05-21 22:01:09 +0300

    correct test_product.py

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
-
+import pytest
 from src.category import Category
-from src.product import Product
+from src.product import Product, Smartphone, LawnGrass
 
 
 def test_out_data_product(input_data_bu_product):
@@ -64,3 +64,71 @@ def test_new_product_creates_new_if_no_duplicate():
 
     assert product in products_list  # Продукт должен быть добавлен в список
     assert len(products_list) == 1
+
+
+def test_smartfone_check(smartphone_item: Smartphone):
+    """Тест инициализации объекта класса Smartphone"""
+    assert smartphone_item.name == "Iphone"
+    assert smartphone_item.description == "Американский Смартфон"
+    assert smartphone_item.price == 12000
+    assert smartphone_item.efficiency == 20.0
+    assert smartphone_item.model == "16 PRO"
+    assert smartphone_item.memory == 512
+    assert smartphone_item.color == "Gold"
+
+
+def test_lawngrass_item_check(lawngrass_item: LawnGrass):
+    """Тест инициализации объекта класса LawnGrass"""
+    assert lawngrass_item.name == "Зелёный ковер"
+    assert lawngrass_item.description == "Газонная трава"
+    assert lawngrass_item.price == 1000
+    assert lawngrass_item.quantity == 500
+    assert lawngrass_item.country == "Россия"
+    assert lawngrass_item.germination_period == "3 месяца"
+    assert lawngrass_item.color == "Зеленая"
+
+
+def test_add_method_smartphone(smartphone_item: Smartphone):
+    """Тест сложения объектов класса Smartphone"""
+    smartphone_new = Smartphone("Iphone", "512GB, Красный цвет, 12MP камера", 300000.0, 4, 19.0, "16Pro", 32, "Red")
+    smartphone_sum = smartphone_new + smartphone_item
+    assert smartphone_sum
+
+
+def test_add_method_smartphone_rise_call():
+    """Тест вызова исключения при сложении вместо продукта или его наследников любой другой объект."""
+    with pytest.raises(TypeError):
+        smartphone1 = Smartphone(
+            "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 11.0, "s20", 8, "green"
+        )
+        smartphone2 = "Неправильная строка"
+        smartphone_sum = smartphone1 + smartphone2
+        assert smartphone_sum
+
+
+def test_add_method_lawngrass(lawngrass_item: LawnGrass):
+    """Тест сложения объектов класса LawnGrass"""
+    lawngrass_new = LawnGrass("Лесной ковёр", "Газонная трава", 1000, 30, "Россия", "2 месяца", "Зеленая")
+
+    lawngrass_sum = lawngrass_new + lawngrass_item
+    assert lawngrass_sum
+
+
+def test_add_method_lawngrass_rise_call():
+    """Тест вызова исключения при сложении вместо продукта или его наследников любой другой объект."""
+    with pytest.raises(TypeError):
+        lawngrass_1 = LawnGrass("Зеленые ковер", "Трава для гольфа", 1500, 2, "Новая Зеландия", "1 неделя", "Синяя")
+        lawngrass_2 = 1
+        lawngrass_sum = lawngrass_1 + lawngrass_2
+        assert lawngrass_sum
+
+
+def test_add_method_rise_call(smartphone_item, lawngrass_item):
+    """Тест вызова исключения при сложении продуктов с разных категорий."""
+    with pytest.raises(TypeError):
+        product1 = smartphone_item
+        product2 = lawngrass_item
+        product3 = "smartphone"
+
+        assert product1 + product2
+        assert product1 + product3
